7a58c216f61208bd83b5f75db35cdc80a1e8fef9
Andreas Waal <andreas.waal@verico.com>
2018-03-09 11:46:42 +0100

    Added test endpoint

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ app.use(bodyParser.json());
 //Routes
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/laws", lawsController.getLaws);
+app.get("/test", function(req, res) {
+  console.log(req.originalUrl);
+  console.log("Who you not work?");
+  res.send("This is not a test");
+});
 
 //Start start
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
