0ed6cd639c899ce73b55c9c89fd73e2337b189d4
SivertRanden <u4lnYi3I>
2018-03-19 11:39:20 +0100

    Made testing stuff

--- app.js
+++ app.js
@@ -16,11 +16,23 @@ app.use(bodyParser.json());
 
 //Routes
 app.get("/", (req, res) => res.send("Hello World!"));
+
 app.get("/laws", lawsController.getLaws);
+
 app.get("/test", function(req, res) {
   console.log(req.originalUrl);
   console.log("Who you not work?");
   res.send("This is not a test");
+});
+
+app.get("/oivind/:id", (req, res) => {
+  res.send("Øivind er en tissemann med en " + req.params.id + " cm lang tiss");
+});
+
+app.post("/posttest", (req, res) => {
+  let testparam = req.query.id;
+  console.log(req);
+  res.send("Hallo? " + testparam);
 });
 
 //Start start
