3dc5d9d618b1f310c0e2065a40829644125c3cd3
AndroidMaker <darrsnoff@gmail.com>
2018-03-21 16:44:09 +0100

    Made the json a bit more smooooooooooth

--- ws/controllers/regulationController.js
+++ ws/controllers/regulationController.js
@@ -1,14 +1,28 @@
 const regulationDao = require("./../dao/regulationDAO.js");
 
 exports.getRegulation = function(req, res) {
-  regulationDao.getRegulationById(req.params.regulation_id, (err, rows) => {
+  regulationDao.getRegulationById(req.params.regulation_id, (err, combinedRows) => {
     if (err) {
       res.sendStatus(500);
     }
-    if (!rows) {
+    if (!combinedRows) {
       res.send("UFFAMEIEN DENNA FORSKRIFTEN FINNES IKKE LOL");
     } else {
-      res.json(rows);
+      regulationDao.getSubsectionsByRegulationId(
+        req.params.regulation_id,
+        combinedRows,
+        (err, combinedRows) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            res.json(combinedRows);
+          }
+        }
+      );
     }
   });
+};
+
+const callback = function(req, res, rows) {
+  res.json(rows);
 };
--- ws/dao/regulationDAO.js
+++ ws/dao/regulationDAO.js
@@ -4,15 +4,32 @@ const db = dao.connection;
 
 exports.getRegulationById = function(id, onDataReceived) {
   db.serialize(() => {
-    db.all(
-      "SELECT r.reg_title, r.date_code, s.title, s.number, s.type FROM regulation AS r, sub_section AS s WHERE r.regulation_id = " +
-        id +
-        " AND s.regulation_id = r.regulation_id",
+    db.get(
+      "SELECT r.reg_title, r.date_code FROM regulation AS r WHERE r.regulation_id = " + id,
       (err, rows) => {
         if (err) {
           console.log(err.message);
         } else {
-          onDataReceived(err, rows);
+          let combinedRows = [];
+          combinedRows.push(rows);
+          onDataReceived(err, combinedRows);
+        }
+      }
+    );
+  });
+};
+
+exports.getSubsectionsByRegulationId = function(id, combinedRows, onDataReceived) {
+  db.serialize(() => {
+    db.all(
+      "SELECT s.title AS subTitle, s.number AS subNumber, s.type AS subType FROM sub_section AS s WHERE s.regulation_id = " +
+        id,
+      (err, rows) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          combinedRows.push(rows);
+          onDataReceived(err, combinedRows);
         }
       }
     );
