23f839a58e0a11a6ee2a65fbcb17bab3b023de78
AndroidMaker <darrsnoff@gmail.com>
2018-03-22 13:58:41 +0100

    Fixed queries for new db

--- ws/dao/regulationDAO.js
+++ ws/dao/regulationDAO.js
@@ -5,7 +5,7 @@ const db = dao.connection;
 exports.getRegulationById = function(id, onDataReceived) {
   db.serialize(() => {
     db.get(
-      "SELECT r.title, r.date_code FROM regulation AS r WHERE r.regulation_id = " + id,
+      "SELECT r.title, r.date_code FROM regulation AS r WHERE r.id_regulation = " + id,
       (err, rows) => {
         if (err) {
           console.log(err.message);
@@ -24,8 +24,9 @@ exports.getRegulationById = function(id, onDataReceived) {
 exports.getSubsectionsByRegulationId = function(id, combinedRows, onDataReceived) {
   db.serialize(() => {
     db.all(
-      "SELECT s.title AS subTitle, s.number AS subNumber, s.type AS subType FROM sub_section AS s WHERE s.regulation_id = " +
-        id,
+      "SELECT s.title AS subTitle, s.number AS subNumber, s.type AS subType FROM regulation AS r, sub_section as s, regulations_sub_sections as rs WHERE rs.regulation_id = " +
+        id +
+        " AND r.id_regulation = rs.regulation_id AND s.id_sub_section = rs.sub_section_id",
       (err, rows) => {
         if (err) {
           console.log(err.message);
