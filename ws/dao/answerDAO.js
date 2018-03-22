7fd46e200a116ac14e816d99ecf0948835f0a83d
AndroidMaker <darrsnoff@gmail.com>
2018-03-22 14:47:03 +0100

    Fixed /categories and /answers. Also fixed consistency in routes.js

--- ws/dao/answerDAO.js
+++ ws/dao/answerDAO.js
@@ -3,7 +3,7 @@ const db = dao.connection;
 
 exports.getAnswerById = function(id, onDataReceived) {
   db.serialize(() => {
-    db.get("SELECT * FROM answer WHERE answer_id = " + id, (err, row) => {
+    db.get("SELECT * FROM answer WHERE id_answer = " + id, (err, row) => {
       if (err) {
         console.log(err.message);
       } else {
@@ -15,7 +15,7 @@ exports.getAnswerById = function(id, onDataReceived) {
 
 exports.getAnswersByCategoryId = function(id, onDataReceived) {
   db.serialize(() => {
-    db.all("SELECT * FROM answer WHERE category_id = " + id, (err, rows) => {
+    db.all("SELECT * FROM answer WHERE id_category = " + id, (err, rows) => {
       if (err) {
         console.log(err.message);
       } else {
@@ -30,7 +30,7 @@ exports.getAnswersByThemeId = function(id, onDataReceived) {
     db.all(
       "SELECT a.* FROM answer as a, themes_answers as ta WHERE (ta.theme_id = " +
         id +
-        " AND a.answer_id = ta.answer_id)",
+        " AND a.id_answer = ta.answer_id)",
       (err, rows) => {
         if (err) {
           console.log(err.message);
--- ws/routes/routes.js
+++ ws/routes/routes.js
@@ -1,13 +1,10 @@
 module.exports = function(app) {
   const lawController = require("../controllers/lawController");
-  const regulationController = require("../controllers/regulationController");
-
   app.route("/laws").get(lawController.getLaws);
   app.route("/laws/:lawId").get(lawController.getLaw);
 
-  app
-    .route("/regulations/:regulation_id")
-    .get(regulationController.getRegulation);
+  const regulationController = require("../controllers/regulationController");
+  app.route("/regulations/:regulation_id").get(regulationController.getRegulation);
 
   const categoryController = require("../controllers/categoryController");
   app.route("/categories").get(categoryController.getCategories);
