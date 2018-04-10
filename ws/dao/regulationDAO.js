d7560c4738a3c9032178a1b9c7d34c9b0c6c775c
AndroidMaker <darrsnoff@gmail.com>
2018-04-10 11:33:33 +0200

    Får tittelen på loven inne på /regulation/id

--- ws/dao/regulationDAO.js
+++ ws/dao/regulationDAO.js
@@ -5,7 +5,9 @@ const db = dao.connection;
 exports.getRegulationById = function(id, onDataReceived) {
   db.serialize(() => {
     db.get(
-      "SELECT r.title, r.date_code FROM regulation AS r WHERE r.id_regulation = " + id,
+      "SELECT r.title, r.date_code, l.title AS lawTitle FROM regulation AS r, law AS l, laws_regulations AS lr WHERE r.id_regulation = " +
+        id +
+        " AND l.id_law = lr.law_id AND r.id_regulation = lr.regulation_id",
       (err, rows) => {
         if (err) {
           console.log(err.message);
