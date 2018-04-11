d03aec28b20027733f3f8511d37309a69bb55eda
Peder Blomvik <149907@stud.hvl.no>
2018-04-11 12:40:14 +0200

    Added retrieving relevant regulation/law and themes connected to a subsection

--- ws/controllers/subsectionController.js
+++ ws/controllers/subsectionController.js
@@ -1,6 +1,7 @@
 const subsectionDAO = require("./../dao/subsectionDAO.js");
 
 exports.getSubsection = function(req, res) {
+  let combinedRows = [];
   subsectionDAO.getSubsectionByID(req.params.subsectionId, (err, row) => {
     if (err) {
       res.sendStatus(500);
@@ -8,7 +9,38 @@ exports.getSubsection = function(req, res) {
     if (!row) {
       res.send("This subsection does not exist!!!");
     } else {
-      res.json(row);
+      combinedRows.push(row);
+      subsectionDAO.getRegulationBySubsectionId(
+        req.params.subsectionId,
+        (err, rows) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            combinedRows.push(rows);
+            subsectionDAO.getLawBySubsectionId(
+              req.params.subsectionId,
+              (err, rows) => {
+                if (err) {
+                  res.sendStatus(500);
+                } else {
+                  combinedRows.push(rows);
+                  subsectionDAO.getThemesBySubsectionId(
+                    req.params.subsectionId,
+                    (err, rows) => {
+                      if (err) {
+                        res.sendStatus(500);
+                      } else {
+                        combinedRows.push(rows);
+                        res.json(combinedRows);
+                      }
+                    }
+                  );
+                }
+              }
+            );
+          }
+        }
+      );
     }
   });
 };
--- ws/dao/subsectionDAO.js
+++ ws/dao/subsectionDAO.js
@@ -5,14 +5,63 @@ const db = dao.connection;
 exports.getSubsectionByID = function(id, onDataReceived) {
   db.serialize(() => {
     db.get(
-      "SELECT s.*, l.title as lawtitle, r.title as regtitle FROM sub_section as s, law as l, regulation as r, regulations_sub_sections as rs, laws_sub_sections as ls  WHERE s.id_sub_section = " +
-        id +
-        " AND ((rs.sub_section_id = s.id_sub_section AND rs.regulation_id = r.id_regulation ) OR (ls.sub_section_id = s.id_sub_section AND ls.law_id = l.id_law))",
+      "SELECT * FROM sub_section as s WHERE s.id_sub_section = " + id,
       (err, row) => {
         if (err) {
           console.log(err.message);
         } else {
           onDataReceived(err, row);
+        }
+      }
+    );
+  });
+};
+
+exports.getRegulationBySubsectionId = function(id, onDataReceived) {
+  db.serialize(() => {
+    db.all(
+      "SELECT r.title as regtitle FROM sub_section as s, regulation as r, regulations_sub_sections as rs WHERE s.id_sub_section = " +
+        id +
+        " AND rs.sub_section_id = s.id_sub_section AND rs.regulation_id = r.id_regulation",
+      (err, rows) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          onDataReceived(err, rows);
+        }
+      }
+    );
+  });
+};
+
+exports.getLawBySubsectionId = function(id, onDataReceived) {
+  db.serialize(() => {
+    db.all(
+      "SELECT l.title as lawtitle FROM sub_section as s, law as l, laws_sub_sections as ls WHERE s.id_sub_section = " +
+        id +
+        " AND ls.sub_section_id = s.id_sub_section AND ls.law_id = l.id_law",
+      (err, rows) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          onDataReceived(err, rows);
+        }
+      }
+    );
+  });
+};
+
+exports.getThemesBySubsectionId = function(id, onDataReceived) {
+  db.serialize(() => {
+    db.all(
+      "SELECT t.short_title FROM theme as t, sub_section as s, themes_sub_sections as ts WHERE s.id_sub_section = " +
+        id +
+        " AND s.id_sub_section = ts.sub_section_id AND t.id_theme = ts.theme_id ",
+      (err, rows) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          onDataReceived(err, rows);
         }
       }
     );
