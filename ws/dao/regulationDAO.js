a7070297883cce04ba1209caa558596bc89533c3
AndroidMaker <darrsnoff@gmail.com>
2018-04-12 13:30:02 +0200

    Fixed a few queries, to actually show everything

--- ws/dao/regulationDAO.js
+++ ws/dao/regulationDAO.js
@@ -26,7 +26,7 @@ exports.getRegulationById = function(id, onDataReceived) {
 exports.getSubsectionsByRegulationId = function(id, combinedRows, onDataReceived) {
   db.serialize(() => {
     db.all(
-      "SELECT s.title AS subTitle, s.number AS subNumber, s.type AS subType FROM regulation AS r, sub_section as s, regulations_sub_sections as rs WHERE rs.regulation_id = " +
+      "SELECT s.title AS subTitle, s.number AS subNumber, s.type AS subType, s.comment AS subComment FROM regulation AS r, sub_section as s, regulations_sub_sections as rs WHERE rs.regulation_id = " +
         id +
         " AND r.id_regulation = rs.regulation_id AND s.id_sub_section = rs.sub_section_id",
       (err, rows) => {
