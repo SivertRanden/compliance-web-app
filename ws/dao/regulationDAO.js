d898933465391fa5727c69c90d27c882ec83b31e
AndroidMaker <darrsnoff@gmail.com>
2018-04-13 13:41:09 +0200

    Fixed queries and testing error handling

--- ws/dao/regulationDAO.js
+++ ws/dao/regulationDAO.js
@@ -5,12 +5,14 @@ const db = dao.connection;
 exports.getRegulationById = function(id, onDataReceived) {
   db.serialize(() => {
     db.get(
-      "SELECT r.title, r.date_code, l.title AS lawTitle FROM regulation AS r, law AS l, laws_regulations AS lr WHERE r.id_regulation = " +
+      "SELECT r.titleTest, r.date_code, l.title AS lawTitle, l.date_code AS lawCode FROM regulation AS r, law AS l, laws_regulations AS lr WHERE r.id_regulation = " +
         id +
         " AND l.id_law = lr.law_id AND r.id_regulation = lr.regulation_id",
       (err, rows) => {
         if (err) {
-          console.log(err.message);
+          //throw new Error("Det skjedde feil i spørringa");
+          console.log("DARRI SE HER, EG ER INNI DEN GREIA HER LOL");
+          //console.log(err.message);
         } else {
           let combinedRows = [];
           if (rows) {
@@ -26,7 +28,7 @@ exports.getRegulationById = function(id, onDataReceived) {
 exports.getSubsectionsByRegulationId = function(id, combinedRows, onDataReceived) {
   db.serialize(() => {
     db.all(
-      "SELECT s.title AS subTitle, s.number AS subNumber, s.type AS subType, s.comment AS subComment FROM regulation AS r, sub_section as s, regulations_sub_sections as rs WHERE rs.regulation_id = " +
+      "SELECT s.* FROM regulation AS r, sub_section as s, regulations_sub_sections as rs WHERE rs.regulation_id = " +
         id +
         " AND r.id_regulation = rs.regulation_id AND s.id_sub_section = rs.sub_section_id",
       (err, rows) => {
