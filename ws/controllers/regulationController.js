1c6636276ef47bcd33e978249ce1d0255df4209a
AndroidMaker <darrsnoff@gmail.com>
2018-04-24 10:53:41 +0200

    Fixed regulationController + DAO not to use callbacks

--- ws/controllers/regulationController.js
+++ ws/controllers/regulationController.js
@@ -7,49 +7,21 @@ const regulationDao = require("./../dao/regulationDAO.js");
 exports.getRegulation = function(req, res) {
   let combinedRows = [];
   regulationDao
-    .getRegulationById(req.params.regulation_id, combinedRows)
-    .then(function(combinedRows) {
-      console.log(combinedRows.length);
+    .getRegulationById(req.params.regulation_id)
+    .then(function(rows) {
+      combinedRows.push(rows);
     })
     .then(
       regulationDao
-        .getSubsectionsByRegulationId(req.params.regulation_id, combinedRows)
-        .then(function(combinedRows) {
-          console.log(combinedRows.length);
-        })
-        .catch(function(err) {
-          console.log(err.message);
-          return;
-        })
-    );
-};
-
-/* 
-    .then(function(combinedRows) {
-      //combinedRows.push(rows);
-      res.json(combinedRows);
-      console.log("SUG EN HESTEKUKK");
-    })
-    .catch(function(err) {
-      res.sendStatus(500);
-      console.log("FØKK MY LIFE");
-      return;
-    })
-    .then(
-      regulationDao
-        .getSubsectionsByRegulationId(req.params.regulation_id, combinedRows)
-        .then(function(combinedRows) {
-          //combinedRows.push(rows);
-          //console.log(combinedRows.length);
+        .getSubsectionsByRegulationId(req.params.regulation_id)
+        .then(function(rows) {
+          combinedRows.push(rows);
           res.json(combinedRows);
-          console.log("SUG PIKK");
         })
         .catch(function(err) {
           res.sendStatus(500);
           console.log(err.message);
           return;
         })
-    )
-    .catch(function(err) {
-      console.log(err.message);
-    }); */
+    );
+};
--- ws/dao/regulationDAO.js
+++ ws/dao/regulationDAO.js
@@ -4,7 +4,7 @@ const db = dao.connection;
 
 //Query the database for a regulation with a given ID and every
 //regulation + subsection connected to that regulation
-exports.getRegulationById = function(id, combinedRows /*, onDataReceived*/) {
+exports.getRegulationById = function(id /*, onDataReceived*/) {
   var promise = new Promise(function(resolve, reject) {
     db.serialize(() => {
       db.get(
@@ -16,8 +16,8 @@ exports.getRegulationById = function(id, combinedRows /*, onDataReceived*/) {
             reject(err.message);
           } else {
             //let combinedRows = [];
-            combinedRows.push(rows);
-            resolve(combinedRows);
+
+            resolve(rows);
           }
         }
       );
@@ -25,27 +25,9 @@ exports.getRegulationById = function(id, combinedRows /*, onDataReceived*/) {
   });
 
   return promise;
-  /*db.serialize(() => {
-    db.get(
-      "SELECT r.title, r.date_code, l.title AS lawTitle, l.date_code AS lawCode FROM regulation AS r, law AS l, laws_regulations AS lr WHERE r.id_regulation = " +
-        id +
-        " AND l.id_law = lr.law_id AND r.id_regulation = lr.regulation_id",
-      (err, rows) => {
-        if (err) {
-          return onDataReceived(err);
-        } else {
-          let combinedRows = [];
-          if (rows) {
-            combinedRows.push(rows);
-          }
-          onDataReceived(err, combinedRows);
-        }
-      }
-    );
-  }); */
 };
 
-exports.getSubsectionsByRegulationId = function(id, combinedRows /*, onDataReceived*/) {
+exports.getSubsectionsByRegulationId = function(id) {
   var promise = new Promise(function(resolve, reject) {
     db.serialize(() => {
       db.all(
@@ -56,7 +38,6 @@ exports.getSubsectionsByRegulationId = function(id, combinedRows /*, onDataRecei
           if (err) {
             reject(err.message);
           } else {
-            combinedRows.push(rows);
             resolve(rows);
           }
         }
@@ -65,20 +46,4 @@ exports.getSubsectionsByRegulationId = function(id, combinedRows /*, onDataRecei
   });
 
   return promise;
-
-  /*db.serialize(() => {
-    db.all(
-      "SELECT s.* FROM regulation AS r, sub_section as s, regulations_sub_sections as rs WHERE rs.regulation_id = " +
-        id +
-        " AND r.id_regulation = rs.regulation_id AND s.id_sub_section = rs.sub_section_id",
-      (err, rows) => {
-        if (err) {
-          return onDataReceived(err);
-        } else {
-          combinedRows.push(rows);
-          onDataReceived(err, combinedRows);
-        }
-      }
-    );
-  }); */
 };
