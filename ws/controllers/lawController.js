bf5cb110b1ad204401678efdf5f9b1dbfe583333
AndroidMaker <darrsnoff@gmail.com>
2018-04-24 12:44:41 +0200

    lawDAO + controller kinda fixed

--- ws/controllers/lawController.js
+++ ws/controllers/lawController.js
@@ -2,50 +2,39 @@ const lawDao = require("./../dao/lawDAO.js");
 
 //Gets all laws and sends all rows in response with JSON
 exports.getLaws = function(req, res) {
-  lawDao.getAllLaws((err, rows) => {
-    if (err) {
+  lawDao
+    .getAllLaws()
+    .then(function(rows) {
+      res.json(rows);
+    })
+    .catch(function(err) {
       res.sendStatus(500);
       console.log(err.message);
       return;
-    } else {
-      res.json(rows);
-    }
-  });
+    });
 };
 
 //Gets the law with corresponding id, regulations with corresponding law id
 //and subsections with corresponding id
 exports.getLaw = function(req, res) {
   let combinedRows = [];
-  lawDao.getLawById(req.params.lawId, (err, row) => {
-    if (err) {
+  lawDao
+    .getLawById(req.params.lawId)
+    .then(function(rows) {
+      combinedRows.push(rows);
+    })
+    .then(lawDao.getRegulationByLawId(req.params.lawId))
+    .then(function(rows) {
+      combinedRows.push(rows);
+    })
+    .then(lawDao.getSubsectionsByLawId(req.params.lawId))
+    .then(function(rows) {
+      combinedRows.push(rows);
+      res.json(combinedRows);
+    })
+    .catch(function(err) {
       res.sendStatus(500);
       console.log(err.message);
       return;
-    }
-    if (!row) {
-      res.sendStatus(404);
-    } else {
-      combinedRows.push(row);
-      lawDao.getRegulationByLawId(req.params.lawId, (err, rows) => {
-        if (err) {
-          res.sendStatus(500);
-          console.log(err.message);
-          return;
-        } else {
-          combinedRows.push(rows);
-          lawDao.getSubsectionsByLawId(req.params.lawId, (err, rows) => {
-            if (err) {
-              res.sendStatus(500);
-              console.log(err.message);
-              return;
-            } else {
-              combinedRows.push(rows);
-              res.json(combinedRows);
-            }
-          });
-        }
-      });
-    }
-  });
+    });
 };
--- ws/dao/lawDao.js
+++ ws/dao/lawDao.js
@@ -1,61 +1,73 @@
 const dao = require("./db.js");
 const db = dao.connection;
 
-exports.getAllLaws = function(onDataReceived) {
-  db.serialize(() => {
-    db.all("SELECT * FROM law", (err, rows) => {
-      if (err) {
-        return onDataReceived(err);
-      } else {
-        onDataReceived(err, rows);
-      }
+exports.getAllLaws = function() {
+  var promise = new Promise(function(resolve, reject) {
+    db.serialize(() => {
+      db.all("SELECT * FROM law", (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
     });
   });
+  return promise;
 };
 
-exports.getLawById = function(id, onDataReceived) {
-  db.serialize(() => {
-    db.get("SELECT * FROM law WHERE id_law = " + id, (err, row) => {
-      if (err) {
-        return onDataReceived(err);
-      } else {
-        onDataReceived(err, row);
-      }
+exports.getLawById = function(id) {
+  var promise = new Promise(function(resolve, reject) {
+    db.serialize(() => {
+      db.get("SELECT * FROM law WHERE id_law = " + id, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
     });
   });
+  return promise;
 };
 
-exports.getRegulationByLawId = function(id, onDataReceived) {
-  db.serialize(() => {
-    db.all(
-      `SELECT r.* FROM regulation as r, law as l, laws_regulations as lr
+exports.getRegulationByLawId = function(id) {
+  var promise = new Promise(function(resolve, reject) {
+    db.serialize(() => {
+      db.all(
+        `SELECT r.* FROM regulation as r, law as l, laws_regulations as lr
       WHERE lr.law_id = ` +
-        id +
-        ` AND l.id_law = lr.law_id AND r.id_regulation = lr.regulation_id`,
-      (err, rows) => {
-        if (err) {
-          return onDataReceived(err);
-        } else {
-          onDataReceived(err, rows);
+          id +
+          ` AND l.id_law = lr.law_id AND r.id_regulation = lr.regulation_id`,
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
         }
-      }
-    );
+      );
+    });
   });
+  return promise;
 };
 
-exports.getSubsectionsByLawId = function(id, onDataReceived) {
-  db.serialize(() => {
-    db.all(
-      "SELECT s.* FROM sub_section as s, law as l, laws_sub_sections AS ls WHERE l.id_law = " +
-        id +
-        " AND l.id_law = ls.law_id AND s.id_sub_section = ls.sub_section_id",
-      (err, rows) => {
-        if (err) {
-          return onDataReceived(err);
-        } else {
-          onDataReceived(err, rows);
+exports.getSubsectionsByLawId = function(id) {
+  var promise = new Promise(function(resolve, reject) {
+    db.serialize(() => {
+      db.all(
+        "SELECT s.* FROM sub_section as s, law as l, laws_sub_sections AS ls WHERE l.id_law = " +
+          id +
+          " AND l.id_law = ls.law_id AND s.id_sub_section = ls.sub_section_id",
+        (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
         }
-      }
-    );
+      );
+    });
   });
+  return promise;
 };
