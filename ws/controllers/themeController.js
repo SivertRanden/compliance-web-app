a660fe2fd4ec0a45605803890f94c3893ca00fcb
AndroidMaker <darrsnoff@gmail.com>
2018-04-24 17:20:53 +0200

    themes uses promises!

--- ws/controllers/themeController.js
+++ ws/controllers/themeController.js
@@ -2,65 +2,60 @@ const themeDAO = require("./../dao/themeDAO.js");
 
 //Gets all themes and sends all rows in response with JSON
 exports.getThemes = function(req, res) {
-  themeDAO.getAllThemes((err, rows) => {
-    if (err) {
+  themeDAO
+    .getAllThemes()
+    .then(function(rows) {
+      res.json(rows);
+    })
+    .catch(function(err) {
       res.sendStatus(500);
       console.log(err.message);
       return;
-    } else {
-      res.json(rows);
-    }
-  });
+    });
 };
 
 //Gets theme by id, also gets associated laws, regulations, answers and implementations
 exports.getThemeById = function(req, res) {
   const themeId = req.params.themeId;
   let combinedRows = [];
-  themeDAO.getThemeById(themeId, (err, row) => {
-    if (err) {
-      res.sendStatus(500);
-      console.log(err.message);
-      return;
-    }
-    if (!row) {
-      res.sendStatus(404);
-    } else {
-      combinedRows.push(row);
-      themeDAO.getLawsByThemeId(themeId, (err, rows) => {
-        if (err) {
-          res.sendStatus(500);
-          console.log(err.message);
-          return;
-        } else {
+
+  themeDAO
+    .getThemeById(themeId)
+    .then(function(rows) {
+      combinedRows.push(rows);
+    })
+    .then(
+      themeDAO
+        .getLawsByThemeId(themeId)
+        .then(function(rows) {
           combinedRows.push(rows);
-          themeDAO.getRegulationsByThemeId(themeId, (err, rows) => {
-            if (err) {
-              res.sendStatus(500);
-              console.log(err.message);
-              return;
-            }
-            combinedRows.push(rows);
-            themeDAO.getAnswersByThemeId(themeId, (err, rows) => {
-              if (err) {
-                res.sendStatus(500);
-                console.log(err.message);
-                return;
-              }
+        })
+        .then(
+          themeDAO
+            .getRegulationsByThemeId(themeId)
+            .then(function(rows) {
               combinedRows.push(rows);
-              themeDAO.getImplementationsByThemeId(themeId, (err, rows) => {
-                if (err) {
-                  res.sendStatus(500);
-                  console.log(err.message);
-                  return;
-                }
-                combinedRows.push(rows);
-                res.json(combinedRows);
-              });
-            });
-          });
-        }
-      });
-    }
-  });
+            })
+            .then(
+              themeDAO
+                .getAnswersByThemeId(themeId)
+                .then(function(rows) {
+                  combinedRows.push(rows);
+                })
+                .then(
+                  themeDAO
+                    .getImplementationsByThemeId(themeId)
+                    .then(function(rows) {
+                      combinedRows.push(rows);
+                      res.json(combinedRows);
+                    })
+                    .catch(function(err) {
+                      res.sendStatus(500);
+                      console.log(err.message);
+                      return;
+                    })
+                )
+            )
+        )
+    );
 };
