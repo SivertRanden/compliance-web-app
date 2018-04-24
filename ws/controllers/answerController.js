f32d281f83bcc15fcc307e82211820d49d203c9c
AndroidMaker <darrsnoff@gmail.com>
2018-04-24 14:06:31 +0200

    answerController + dao uses promises

--- ws/controllers/answerController.js
+++ ws/controllers/answerController.js
@@ -1,48 +1,52 @@
 const answerDao = require("./../dao/answerDAO.js");
 
 exports.getAnswer = function(req, res) {
-  answerDao.getAnswerById(req.params.answerId, (err, row) => {
-    if (err) {
+  answerDao
+    .getAnswerById(req.params.answerId)
+    .then(function(rows) {
+      res.json(rows);
+    })
+    .catch(function(err) {
       res.sendStatus(500);
       console.log(err.message);
       return;
-    }
-    if (!row) {
-      res.sendStatus(404);
-    } else {
-      res.json(row);
-    }
-  });
+    });
 };
 
 exports.getAnswersByParameter = function(req, res) {
   if (req.query.categoryId && !req.query.themeId) {
-    answerDao.getAnswersByCategoryId(req.query.categoryId, (err, rows) => {
-      if (err) {
+    answerDao
+      .getAnswersByCategoryId(req.query.categoryId)
+      .then(function(rows) {
+        if (rows) {
+          res.send("Det finnes ingen svar i aktuell kategori.");
+          return;
+        } else {
+          res.json(rows);
+        }
+      })
+      .catch(function(err) {
         res.sendStatus(500);
         console.log(err.message);
         return;
-      }
-      if (!rows) {
-        res.send("Det finnes ingen svar i aktuell kategori.");
-      } else {
-        res.json(rows);
-      }
-    });
+      });
   } else if (req.query.themeId && !req.query.categoryId) {
-    answerDao.getAnswersByThemeId(req.query.themeId, (err, rows) => {
-      if (err) {
+    answerDao
+      .getAnswersByThemeId(req.query.themeId)
+      .then(function(rows) {
+        if (!rows) {
+          res.send("Det finnes ingen svar i aktuelt tema");
+          return;
+        } else {
+          res.json(rows);
+        }
+      })
+      .catch(function(err) {
         res.sendStatus(500);
         console.log(err.message);
         return;
-      }
-      if (!rows) {
-        res.send("Det finnes ingen svar i aktuelt tema.");
-      } else {
-        res.json(rows);
-      }
-    });
+      });
   } else {
-    res.send("Det finnes ingen data med angitt parameter.");
+    res.sendStatus(404);
   }
 };
