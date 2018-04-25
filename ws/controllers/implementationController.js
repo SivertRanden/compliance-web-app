95482dce3d724027de2751fd623615028ea45e38
AndroidMaker <darrsnoff@gmail.com>
2018-04-25 11:05:37 +0200

    litta kommentar + if

--- ws/controllers/implementationController.js
+++ ws/controllers/implementationController.js
@@ -1,26 +1,32 @@
 const implementationDao = require("./../dao/implementationDAO.js");
 
+//SE PÅ BRUKEN AV IF-SETNINGEN HER! DET VAR LITT STØGT
 exports.getImplementation = function(req, res) {
   let combinedRows = [];
-  implementationDao
-    .getImplementationByThemeId(req.query.themeId)
-    .then(function(rows) {
-      combinedRows.push(rows);
-    })
-    .then(
-      implementationDao.getKeyPersonsByThemeId(req.query.themeId).then(function(rows) {
+  if (req.query.themeId) {
+    implementationDao
+      .getImplementationByThemeId(req.query.themeId)
+      .then(function(rows) {
         combinedRows.push(rows);
       })
-    )
-    .then(
-      implementationDao.getTrainingPersonsByThemeId(req.query.themeId).then(function(rows) {
-        combinedRows.push(rows);
-        res.json(combinedRows);
-      })
-    )
-    .catch(function(err) {
-      res.sendStatus(500);
-      console.log(err.message);
-      return;
-    });
+      .then(
+        implementationDao.getKeyPersonsByThemeId(req.query.themeId).then(function(rows) {
+          combinedRows.push(rows);
+        })
+      )
+      .then(
+        implementationDao.getTrainingPersonsByThemeId(req.query.themeId).then(function(rows) {
+          combinedRows.push(rows);
+          res.json(combinedRows);
+        })
+      )
+      .catch(function(err) {
+        res.sendStatus(500);
+        console.log(err.message);
+        return;
+      });
+  } else {
+    res.sendStatus(404);
+    return;
+  }
 };
