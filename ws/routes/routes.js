611077d4bd2d042ffb47f570c52cd91f00682004
AndroidMaker <darrsnoff@gmail.com>
2018-04-25 12:07:14 +0200

    Fixed import statements in routes.js

--- ws/routes/routes.js
+++ ws/routes/routes.js
@@ -1,25 +1,33 @@
 module.exports = function(app) {
   const lawController = require("../controllers/lawController");
+  const regulationController = require("../controllers/regulationController");
+  const categoryController = require("../controllers/categoryController");
+  const answerController = require("../controllers/answerController");
+  const subsectionController = require("../controllers/subsectionController");
+  const themeController = require("../controllers/themeController");
+  const implementationController = require("../controllers/implementationController");
+
+  //laws
   app.route("/laws").get(lawController.getLaws);
   app.route("/laws/:lawId").get(lawController.getLaw);
 
-  const regulationController = require("../controllers/regulationController");
+  //Regulations
   app.route("/regulations/:regulation_id").get(regulationController.getRegulation);
 
-  const categoryController = require("../controllers/categoryController");
+  //Categories
   app.route("/categories").get(categoryController.getCategories);
 
-  const answerController = require("../controllers/answerController");
+  //Answers
   app.route("/answers/:answerId").get(answerController.getAnswer);
   app.route("/answers").get(answerController.getAnswersByParameter);
 
-  const subsectionController = require("../controllers/subsectionController");
+  //Subsections
   app.route("/subsections/:subsectionId").get(subsectionController.getSubsection);
 
-  const themeController = require("../controllers/themeController");
+  //Themes
   app.route("/themes").get(themeController.getThemes);
   app.route("/themes/:themeId").get(themeController.getThemeById);
 
-  const implementationController = require("../controllers/implementationController");
+  //Implementations
   app.route("/implementations").get(implementationController.getImplementation);
 };
