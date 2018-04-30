6e13db1e1e37e32dc9d98f2edecfe81239a19f3a
SivertRanden <u4lnYi3I>
2018-04-30 10:32:21 +0200

    fixed theme query

--- ws/dao/themeDAO.js
+++ ws/dao/themeDAO.js
@@ -19,16 +19,13 @@ exports.getAllThemes = function() {
 exports.getThemeById = function(id) {
   var promise = new Promise(function(resolve, reject) {
     db.serialize(() => {
-      db.get(
-        "SELECT * FROM theme WHERE id_theme = " + escape(id),
-        (err, rows) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(rows);
-          }
+      db.get("SELECT * FROM theme WHERE id_theme = " + escape(id), (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
         }
-      );
+      });
     });
   });
   return promise;
@@ -38,7 +35,7 @@ exports.getLawsByThemeId = function(id) {
   var promise = new Promise(function(resolve, reject) {
     db.serialize(() => {
       db.all(
-        `SELECT l.title, l.date_code FROM law as l, theme as t, laws_sub_sections as ls, themes_sub_sections as ts
+        `SELECT l.* FROM law as l, theme as t, laws_sub_sections as ls, themes_sub_sections as ts
       WHERE t.id_theme = ` +
           escape(id) +
           ` AND l.id_law = ls.law_id AND ls.sub_section_id = ts.sub_section_id AND ts.theme_id = t.id_theme`,
@@ -59,7 +56,7 @@ exports.getRegulationsByThemeId = function(id) {
   var promise = new Promise(function(resolve, reject) {
     db.serialize(() => {
       db.all(
-        `SELECT r.title, r.date_code FROM regulation as r, theme as t, regulations_sub_sections as rs, themes_sub_sections as ts
+        `SELECT r.* FROM regulation as r, theme as t, regulations_sub_sections as rs, themes_sub_sections as ts
       WHERE t.id_theme = ` +
           escape(id) +
           ` AND r.id_regulation = rs.regulation_id AND ts.theme_id = t.id_theme AND rs.sub_section_id = ts.sub_section_id`,
@@ -80,7 +77,7 @@ exports.getAnswersByThemeId = function(id) {
   var promise = new Promise(function(resolve, reject) {
     db.serialize(() => {
       db.all(
-        `SELECT a.title, a.status FROM answer as a, theme as t, themes_answers as ta
+        `SELECT a.* FROM answer as a, theme as t, themes_answers as ta
       WHERE t.id_theme = ` +
           escape(id) +
           ` AND a.id_answer = ta.answer_id AND t.id_theme = ta.theme_id;`,
